d7fc805cef0282360443ddbd8419e3a3d036f858
Hui <jeffrey.x.hui@gmail.com>
2020-02-03 11:15:45 -0800

    conditional dbconfig to account for herokus env vars

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
-const db = require('knex')(require('../knexfile.js')[process.env.DB_ENV]);
+const db = () => {
+  if(process.env.DATABASE_URL) {
+    return require('knex')(require('../knexfile.js')[process.env.DATABASE_URL])
+  } else {
+    return require('knex')(require('../knexfile.js')[process.env.DB_ENV])
+  }
+}
 
-module.exports = db;
+module.exports = db();
