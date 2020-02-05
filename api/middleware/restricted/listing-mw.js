97afc89f5d70387e93a290053b18322ee0f46c71
Hui <jeffrey.x.hui@gmail.com>
2020-02-04 17:27:31 -0800

    added data parser to make sure inputs are coming in with the correct data type

--- api/middleware/restricted/listing-mw.js
+++ api/middleware/restricted/listing-mw.js
@@ -3,7 +3,8 @@
 const addListingMw = [
   listing__payloadExists,
   // listing__addressPresent,
-  listing__hostIdPresent
+  listing__hostIdPresent,
+  cleanData
 ]
 
 module.exports = {
@@ -19,25 +20,31 @@ function listing__payloadExists(req, res, next) {
   }
 }
 
-//constraints
-// function listing__addressPresent(req, res, next) {
-//   if(
-//       req.body.street && 
-//       req.body.city && 
-//       req.body.state && 
-//       req.body.zip
-//     ) {
-//       next();
-//     } else {
-//       res.status(400).json({ error: `missing required address credentials` })
-//     }
-  
-// }
-
 function listing__hostIdPresent(req, res, next) {
   if(req.body.host_id) {
     next();
   } else {
     res.status(400).json({ error: `missing required host_id` })
   }
+}
+
+function cleanData(req, res, next) {
+  req.body.image = req.body.image.toString();
+  req.body.street = req.body.street.toString();
+  req.body.city = req.body.city.toString();
+  req.body.state = req.body.state.toString();
+  req.body.zip = req.body.zip.toString();
+  req.body.room_type = req.body.room_type.toString();
+  req.body.neighborhood = req.body.neighborhood.toString();
+
+  req.body.bedrooms = parseInt(req.body.bedrooms);
+  req.body.bathrooms = parseInt(req.body.bathrooms);
+  req.body.beds = parseInt(req.body.beds);
+  req.body.availability = parseInt(req.body.availability);
+
+  req.body.deposit = parseFloat(req.body.deposit);
+  req.body.cleaning_fee = parseFloat(req.body.cleaning_fee);
+  req.body.host_id = parseFloat(req.body.host_id);
+
+  next();
 }
